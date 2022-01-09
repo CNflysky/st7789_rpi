268d9f19bdb29c66afcf9e5cb7287ae7a9d247b4
CNflysky <CNflysky@qq.com>
2022-01-09 15:14:35 +0800

    FIX: add st7789_spi_close prototype

--- include/st7789_spi.h
+++ include/st7789_spi.h
@@ -11,5 +11,6 @@ void st7789_spi_write_8bit(st7789_t *st7789, datatypes_t type, uint8_t data);
 void st7789_spi_write_16bit(st7789_t *st7789, uint16_t data);
 void st7789_spi_write_multi(st7789_t *st7789, uint8_t *data, uint16_t len);
 void st7789_spi_write_multi16(st7789_t *st7789, uint16_t *data, uint32_t len);
+void st7789_spi_close(st7789_t *st7789);
 
 #endif
